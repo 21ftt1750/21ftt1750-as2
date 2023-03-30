0cc0fdafbb4cd6de3ad716265564986df6ac5341
21ftt1750 <21ftt1750@student.pb.edu.bn>
2023-03-30 10:23:30 +0800

    done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,7 @@
 
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex justify-center">
-                   as2
+                   done
                 </div>
 
                 <div class="mt-16">
